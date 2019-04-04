ec77fd66ffaa25bbc9d44f6eb8a1ad6466f62e01
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 23:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each {|ele| return ele.capitalize}
+  final = []
+  planeteer_calls.each {|ele| final << "#{ele.capitalize}!"}
+  return final
 end
 
 def long_planeteer_calls(words)
